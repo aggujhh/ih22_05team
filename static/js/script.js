b82526e53406644bfccf6bd53a9589e015390db7
aggujhh <121200909+aggujhh@users.noreply.github.com>
2024-10-23 02:02:25 +0900

    111

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,7 @@
 console.log("script.js");
 
-function datejapanese(pubdate) {
-    const date = new Date(pubdate);
-    const pubdateTxt = date.getFullYear() + "年"
-        + (date.getMonth() + 1) + "月"
-        + date.getDate() + "日 "
-        + date.getHours() + "時"
-        + date.getMinutes() + "分"
-        + date.getSeconds() + "秒";
-    return pubdateTxt;
-}
+const logo = document.querySelector("#logo-container")
+logo.addEventListener("click",()=>window.location.href = '/')
 
 // テキストボックスのborder色チェンジ
 const inputList = document.querySelectorAll('[type=text]')

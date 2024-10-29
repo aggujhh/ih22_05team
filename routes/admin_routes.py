ab15db45ac56dfb05510b1e867b4c4f9951cd8eb
Miyajino <51252349+Miyajino@users.noreply.github.com>
2024-10-29 16:39:06 +0900

    ユーザ側と管理者側とで分けた

--- routes/admin_routes.py
+++
@@ -1,5 +0,0 @@
-from . import app, global_data
-from flask import render_template, request, flash, redirect
-from severs.flask_login import Flask_login
-from flask_login import current_user, login_required, logout_user
-

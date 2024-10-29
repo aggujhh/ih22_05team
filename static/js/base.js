03f4f5f18c286feb842e4ee8c082fb1ac37dfc85
aggujhh <121200909+aggujhh@users.noreply.github.com>
2024-10-29 21:24:20 +0900

    chen

--- static/js/base.js
+++ static/js/base.js
@@ -97,6 +97,75 @@ if (textarea) {
     })
 }
 
+const chat_title = document.querySelector("#chat_box .chat_title")
+const chars = document.querySelectorAll("#chat_box .chat_title > div")
+const first_char = document.querySelector("#chat_box .chat_title > div:first-of-type")
+const chat_box = document.querySelector("#chat_box")
+const chat_area = document.querySelector("#chat_box .chat_area")
+let chat_flag = true
+
+chat_title.addEventListener("click", () => {
+    if (chat_flag) {
+        chat_box.style.height = "800px"
+        setTimeout(function () {
+            chat_box.style.width = "400px"
+            chat_title.style.transform = "translateX(180px) translateY(-40px) rotate(-90deg)"
+        }, 600);
+        setTimeout(function () {
+            chars.forEach(e => {
+                e.style.transform = "rotate(90deg)"
+            })
+            first_char.style.transform = "rotate(0deg)"
+            chat_area.style.opacity = 1
+        }, 800);
+        chat_flag = false
+    } else {
+        chat_area.style.opacity = 0
+        chat_title.style.transform = "translateX(0) translateY(0) rotate(0)"
+        chars.forEach(e => {
+            e.style.transform = "rotate(0deg)"
+        })
+        setTimeout(function () {
+            chat_box.style.width = "40px"
+        }, 600);
+        setTimeout(function () {
+            chat_box.style.height = "200px"
+        }, 1200);
+        chat_flag = true
+    }
+})
 
 
+const chat_user_box = document.querySelector("#chat_box .chat_user_box")
+const arrow = document.querySelector("#chat_box .chat_user_box_btn")
+const chat_user_icon = document.querySelector("#chat_box .chat_user_box img")
+const users = document.querySelector("#chat_box .chat_user_box .users")
+const add_user = document.querySelector("#chat_box .chat_user_box .add_user")
+let chat_user_flag = true
+arrow.addEventListener("click", () => {
+    if (chat_user_flag) {
+        chat_user_box.style.width = "82px"
+        chat_user_flag = false
+        arrow.innerHTML = "<p>◀</p>"
+        chat_user_icon.style.opacity = 0
+        users.style.opacity = 1
+        add_user.style.opacity = 1
+    } else {
+        chat_user_box.style.width = "20px"
+        chat_user_flag = true
+        users.style.opacity = 0
+        chat_user_icon.style.opacity = 1
+        arrow.innerHTML = "<p>▶</p>"
+        add_user.style.opacity = 0
+    }
+})
+const add_user_area = document.querySelector("#chat_box .add_user_area")
+document.querySelector("#chat_box .close_btn")
+    .addEventListener("click", (e) => {
+    add_user_area.style.transform = "translateY(100%)"
+})
+
+add_user.addEventListener("click", () => {
+    add_user_area.style.transform = "translateY(0)"
+})
 

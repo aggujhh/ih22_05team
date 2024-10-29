5f14aff9c24b78757ef4d21209b99781a2679824
Miyajino <51252349+Miyajino@users.noreply.github.com>
2024-10-29 17:42:52 +0900

    miyamoto

--- front/app.py
+++ front/app.py
@@ -1,8 +1,9 @@
 # flaskより必要なモジュールをインポートする
 from routes import app
+from config import HOST, PORT
 import os
 
 
 # アプリケーションの実行
 if __name__ == '__main__':
-    app.run()
+    app.run(host=HOST, port=PORT)

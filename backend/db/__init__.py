6ad642d4d2b55dcdaa2a945119e3dc7f7aa42034
aggujhh <121200909+aggujhh@users.noreply.github.com>
2024-11-14 10:14:24 +0900

    chen

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -12,6 +12,8 @@ class SqlPool(object):
             blocking=True,  # 接続プールに利用可能な接続がない場合、待機するかどうか
             host='localhost',
             port=3306,
+            user='root',
+            password='',
             database='ih22_db',
             charset='utf8',
             autocommit=True  # データを自動コミットする

9eaea010f6a92f50cc70ea5a312f738611f53e57
aggujhh <121200909+aggujhh@users.noreply.github.com>
2024-12-02 13:34:04 +0900

    chen

--- db.sql
+++ db.sql
@@ -991,10 +991,9 @@ ALTER TABLE expertise
     ADD category_name char(60);
 
 -- design_previewの変更
+ALTER TABLE design_preview DROP PRIMARY KEY;
 ALTER TABLE design_preview
     DROP image_id;
+ALTER TABLE design_preview ADD PRIMARY KEY (user_id);
 ALTER TABLE design_preview
-    change image_url images varchar(255);
-ALTER TABLE design_preview
-    change image_url images varchar(255);
-ALTER TABLE design_preview MODIFY images TEXT;
+    change image_url images TEXT;
--- front/severs/flask_mail.py
+++ front/severs/flask_mail.py
@@ -5,7 +5,7 @@ from routes import app
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587  # TLSは587、SSLなら465
 app.config['MAIL_USERNAME'] = 'chinntaro31@gmail.com'
-app.config['MAIL_PASSWORD'] = 'qmwwwvxzmxicrkag'  # GmailのApp用のmパスワード設定をしておく必要あり
+app.config['MAIL_PASSWORD'] = 'rtvxujdnsiogpvpa'  # GmailのApp用のmパスワード設定をしておく必要あり
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_DEFAULT_SENDER'] = 'chinntaro31@gmail.com'  # これがあるとsender設定が不要になる
